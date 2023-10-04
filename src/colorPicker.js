5dd9bd9488b1cd343b29fcc67075218a058a637e
Emmanuel <endiesworld@gmail.com>
2023-10-04 04:51:49 +0100

    feat: Added colo picker project

---
+++ src/colorPicker.js
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+import './colorPicker.css'
+
+function ColorPicker() {
+    const [colors, setColors] = useState([
+        '#FFD500',
+        '#FF0040',
+    ]);
+    
+    const colorStops = colors.join(', ');
+    const backgroundImage = `linear-gradient(${colorStops})`;
+
+    return (
+        <>
+        <div
+            className="gradient-preview"
+            style={{
+            backgroundImage,
+            }}
+        />
+        
+        <form>
+            {colors.map((color, index) => {
+            const colorId = `color-${index}`;
+            
+            return (
+                <div key={colorId} className="color-row">
+                <label htmlFor={colorId}>
+                    Color {index + 1}:
+                </label>
+                <input
+                    id={colorId}
+                    type="color"
+                    value={color}
+                    onChange={event => {
+                    let nextColors = [...colors];
+                    nextColors[index] = event.target.value;
+                    
+                    setColors(nextColors);
+                    }}
+                />
+                </div>
+            );
+            })}
+        </form>
+        </>
+    );
+}
+
+export default ColorPicker;
