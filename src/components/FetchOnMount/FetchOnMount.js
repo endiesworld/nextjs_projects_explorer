4ab6decf181be316ba3f293b8121cc7ca2d24116
Emmanuel <endiesworld@gmail.com>
2023-11-17 05:33:45 +0100

    feat: Added isloading capability in fetchonmount component' '

--- src/components/FetchOnMount/FetchOnMount.js
+++ src/components/FetchOnMount/FetchOnMount.js
@@ -6,17 +6,31 @@ import style from './fetch.module.css';
 const ENDPOINT =
   'https://jor-test-api.vercel.app/api/get-temperature';
 
+
+// Remember: we manage the network request! Our fetcher function is responsible for retrieving the data, and 
+// passing it along to SWR. If we want this to count as an error, we need to throw it:
+
 async function fetcher(endpoint) {
   const response = await fetch(endpoint);
   const json = await response.json();
-  
+
+  if (!json.ok) {
+    throw json;
+  }
+
   return json;
 }
 
 function FetchOnMount() {
-  const { data, error } = useSWR(ENDPOINT, fetcher);
-  
-  console.log(data, error);
+  const { data, isLoading, error } = useSWR(ENDPOINT, fetcher);
+
+  if (isLoading) {
+    return <p className={style.wrapper}>Loading…</p>;
+  }
+
+  if (error) {
+    return <p className={style.wrapper}>Something's gone wrong</p>;
+  }
   
   return (
     <p className={style.wrapper}>
